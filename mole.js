74961dd59d27e9904d6cc790806590f8a6647a85
danny pong <dannybankx@dannys-MacBook-Air.local>
2020-10-08 16:03:19 -0400

    pop up mole and hide mole

--- mole.js
+++ mole.js
@@ -12,7 +12,27 @@
 //   }, 1000);
 // });
 
-function popUpRandomMole{
-    
+function popUpRandomMole() {
+  let moleHeads = document.querySelectorAll(".wgs__mole-head");
+  let randomNum = Math.floor(Math.random() * moleHeads.length);
+  let currentMole = moleHeads[randomNum];
+
+  currentMole.classList.remove("wgs__mole-head--hidden");
+
+  setTimeout(() => {
+    hideMole(currentMole);
+  }, 1000);
 }
-document.querySelectorAll('wgs__mole-head')
+
+function hideMole(currentMole) {
+  currentMole.classList.add("wgs__mole-head--hidden");
+  setTimeout(() => {
+    popUpRandomMole();
+  }, 1000);
+}
+
+// function getRandomNum(min, max) {
+//   min = Math.ceil(min);
+//   max = Math.floor(max);
+//   return Math.floor(Math.random() * (max - min + 1) + min);
+// }
