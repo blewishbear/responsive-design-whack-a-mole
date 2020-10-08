1e5e471728a46f544bc202c7a1d03667bc948dad
danny pong <dannybankx@dannys-MacBook-Air.local>
2020-10-08 13:46:22 -0400

    animated hiding moles

--- mole.js
+++ mole.js
@@ -1,1 +1,13 @@
 // Write your JS here.
+window.addEventListener("DOMContentLoaded", () => {
+  //set interval of one second
+  setInterval(() => {
+    //select ALL mole head classes and store into variable
+    const moleHeads = document.querySelectorAll(".wgs__mole-head");
+    //iterate through key of obj, grabbing each molehead image
+    for (let moleHead of moleHeads) {
+      //if class is hidden, toggle it off. vice versa
+      moleHead.classList.toggle("wgs__mole-head--hidden");
+    }
+  }, 1000);
+});
