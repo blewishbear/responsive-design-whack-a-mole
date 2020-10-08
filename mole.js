152c6759033903fa65d8bffbd2a3d4b4643bc8b5
Mathias <mathiasanderson88@gmail.com>
2020-10-08 12:40:42 -0700

    Finishing phase 1

--- mole.js
+++ mole.js
@@ -1,13 +1,18 @@
-// Write your JS here.
-window.addEventListener("DOMContentLoaded", () => {
-  //set interval of one second
-  setInterval(() => {
-    //select ALL mole head classes and store into variable
-    const moleHeads = document.querySelectorAll(".wgs__mole-head");
-    //iterate through key of obj, grabbing each molehead image
-    for (let moleHead of moleHeads) {
-      //if class is hidden, toggle it off. vice versa
-      moleHead.classList.toggle("wgs__mole-head--hidden");
-    }
-  }, 1000);
-});
+// // Write your JS here.
+// window.addEventListener("DOMContentLoaded", () => {
+//   //set interval of one second
+//   setInterval(() => {
+//     //select ALL mole head classes and store into variable
+//     const moleHeads = document.querySelectorAll(".wgs__mole-head");
+//     //iterate through key of obj, grabbing each molehead image
+//     for (let moleHead of moleHeads) {
+//       //if class is hidden, toggle it off. vice versa
+//       moleHead.classList.toggle("wgs__mole-head--hidden");
+//     }
+//   }, 1000);
+// });
+
+function popUpRandomMole{
+    
+}
+document.querySelectorAll('wgs__mole-head')
